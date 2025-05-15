d7e059bfe5e647ab282654a6f225a9f842eb18a2
Ryan Layer <ryan.layer@gmail.com>
2025-05-15 08:47:26 -0600

    fix bug

--- vcf_encode/src/get_segment_sample_r2.py
+++ vcf_encode/src/get_segment_sample_r2.py
@@ -54,7 +54,7 @@ def get_segment_data(file, sample_pop_map=None):
             items = line.strip().split()
             if items[0] not in dists:
                 dists[items[0]] = []
-            if pop_map is not None and sample_pop_map is not None:
+            if sample_pop_map is not None:
                 if sample_pop_map[items[0]][1] == sample_pop_map[items[1]][1]:
                     dists[items[0]].append(float(items[2]))
             else:
@@ -64,14 +64,21 @@ def get_segment_data(file, sample_pop_map=None):
 def main():
     args = get_args()
 
-    gt_dists = get_segment_data(args.gt)
-    emb_dists = get_segment_data(args.emb)
 
-    pop_map = None
     sample_pop_map = None
     if args.pop_map is not None and args.pop_def is not None:
         pop_map = get_pop_map(args.pop_map)
         sample_pop_map = get_sample_pop_map(args.pop_def, pop_map)
+
+    
+    gt_dists = None
+    emb_dists = None
+    if sample_pop_map is not None:
+        gt_dists = get_segment_data(args.gt, sample_pop_map=sample_pop_map)
+        emb_dists = get_segment_data(args.emb, sample_pop_map=sample_pop_map)
+    else:
+        gt_dists = get_segment_data(args.gt)
+        emb_dists = get_segment_data(args.emb)
 
     names = list(gt_dists.keys())
 
